32234581632efd80a0a07edb9d946a9d0933be50
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2022-02-10 08:05:36 -0300

    feat: add ReCaptcha v3

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { GoogleReCaptchaProvider, GoogleReCaptcha } from 'react-google-recaptcha-v3'
 
 import Logo from '../../assets/TechAcademy.svg'
 import CodeImage from '../../assets/code-image-2.svg'
@@ -19,8 +20,10 @@ import {
   InputContainer,
 
 } from './styles'
+import { useState } from 'react'
 
 export default function Signup() {
+  const [token, setToken] = useState('')
 
   const router = useRouter()
 
@@ -29,52 +32,59 @@ export default function Signup() {
   }
 
   return (
-    <Container>
-      <ImageContainer id='image-container'>
-        <Logo />
-        <CodeImage />
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPCHA_KEY}
+    >
+      <Container>
+        <ImageContainer id='image-container'>
+          <Logo />
+          <CodeImage />
 
-        <Text>
-          Crie uma conta e desfrute tudo o que <br ></br>
-          podemos oferecer
-        </Text>
+          <Text>
+            Crie uma conta e desfrute tudo o que <br ></br>
+            podemos oferecer
+          </Text>
 
-      </ImageContainer>
+        </ImageContainer>
 
-      <LoginContainer>
-        <Header>Login</Header>
+        <LoginContainer>
+          <Header>Login</Header>
 
-        <LoginContent>
-          <Form>
-            <InputContainer>
-              <InputForm type='text' placeholder='Digite seu nome' />
-              <InputForm type='email' placeholder='Digite um email' />
-              <InputForm type='email' placeholder='Confirme o email' />
-              <InputForm type='password' placeholder='Digite uma senha' />
-              <InputForm type='password' placeholder='Confirme a senha' />
-            </InputContainer>
+          <LoginContent>
+            <Form>
+              <InputContainer>
+                <InputForm type='text' placeholder='Digite seu nome' />
+                <InputForm type='email' placeholder='Digite um email' />
+                <InputForm type='email' placeholder='Confirme o email' />
+                <InputForm type='password' placeholder='Digite uma senha' />
+                <InputForm type='password' placeholder='Confirme a senha' />
+              </InputContainer>
 
-            <SimpleButton
-              title='Criar conta'
+              <SimpleButton
+                title='Criar conta'
+              />
+            </Form>
+
+            <HaveAccountText>
+              Já tem uma conta? Faça o
+
+              <HaveAccountButton
+                onClick={handleGoToHome}
+              >
+                Login
+              </HaveAccountButton>
+
+            </HaveAccountText>
+
+            <GoogleReCaptcha
+              onVerify={token => setToken(token)}
             />
-          </Form>
 
-          <HaveAccountText>
-            Já tem uma conta? Faça o
-
-            <HaveAccountButton
-              onClick={handleGoToHome}
-            >
-              Login
-            </HaveAccountButton>
-
-          </HaveAccountText>
+          </LoginContent>
 
 
-        </LoginContent>
-
-
-      </LoginContainer>
-    </Container>
+        </LoginContainer>
+      </Container>
+    </GoogleReCaptchaProvider>
   )
 }
